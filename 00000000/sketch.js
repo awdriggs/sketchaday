5c40d545195880c4635de794b3fb3d00479b42af
Adam Driggers <awdriggs@gmail.com>
2024-09-09 19:45:48 +0200

    0905

--- 00000000/sketch.js
+++ 00000000/sketch.js
@@ -2,10 +2,12 @@ function setup() {
   // createCanvas(400, 400);
   createCanvas(windowWidth, windowHeight);
 
+  
 }
 
 function draw() {
-  background(220);
+  background(255);
+
 }
 
 function windowResized() {
---
+++ d09092024/sketch.js
@@ -0,0 +1,35 @@
+// after Kitty van der Mijll Dekker
+// https://www.stedelijk.nl/nl/collectie/16410-kitty-van-der-mijll-dekker-glasdoek-progressieve-randen-in-geel-en-groen-%28dessinnr.-400%29#content
+
+console.log("hello");
+
+function setup() {
+  // createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+
+  
+}
+
+function draw() {
+  background(255);
+  
+  for(let i = 1; i < width/3; i+= i * 0.5){
+    stroke('gold');
+    line(i, 0, i, height);
+    // line(0, i, width, i);
+    stroke('green');
+    line(width - i, 0, width - i, height);
+  }
+
+  for(let i = 1; i < height/3; i += i * 0.5){
+    stroke('gold');
+    line(0, i, width, i);
+    // line(i, 0, i, height);
+    stroke('green');
+    line(0, height - i, width, height - i);
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
