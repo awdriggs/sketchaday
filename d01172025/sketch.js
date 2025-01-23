f99d08c62ddf8f1843b637441ed8d513794ab2ce
Adam Driggers <awdriggs@gmail.com>
2025-01-23 10:27:32 +0100

    update

---
+++ d01172025/sketch.js
@@ -0,0 +1,40 @@
+let offsetX = 0;
+let offsetY = 0;
+let max;// = 200;
+
+function setup() {
+  createCanvas(800, 800);
+  max = width/4;
+  print("working");
+  noStroke();
+  background(0);
+}
+
+function draw() {
+  fill(255, 100);
+  let x = cos(offsetX) * max + width/2;
+  let y = sin(offsetY) * max + height/2;
+
+  ellipse(x, y, 2, 2);
+
+  if(x == 0){
+    print(boom);
+  }
+
+  offsetX += random(0.05);
+  offsetY += random(0.05);
+
+  if(frameCount % 100 == 0){
+    max += random(-10, 10);
+     
+    console.log("boom");
+    // offSetX = 0;
+  }
+
+}
+
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
