95cecc3f7289d99b4c75db315b4fd38a4c2db32c
Adam Driggers <awdriggs@gmail.com>
2025-05-01 12:14:45 +0200

    update

---
+++ sketches/d04302025/sketch.js
@@ -0,0 +1,89 @@
+let cells = [];
+let wave = [];
+
+function setup() {
+  createCanvas(800, 800);
+  // createCanvas(windowWidth, windowHeight);
+  stroke("red");
+
+  let numRows = 50;
+  let numCols = 50;
+  let sqWidth = width/numCols;
+  let sqHeight = height/numRows;
+  for(let i = 0; i < numRows; i++){
+    for(let j = 0; j < numCols; j++){
+      cells.push(new Cell(j * sqWidth, i * sqHeight, sqWidth, sqHeight))
+    }
+  }
+}
+
+function draw() {
+  background(255);
+
+  for(let i = 0; i < width; i++){
+    let x = i;
+    let y = cos(i/80) * 200 + width/2;
+    wave.push({x: x, y: y});
+    // point(x,y)
+  }
+   
+
+  stroke(0);
+  for(let c of cells){
+    c.checkForFill(wave);
+    c.draw();
+  }
+  
+  //draw sine wave
+  stroke("red")
+  for(let p of wave){
+    ellipse(p.x, p.y, 2, 2);
+  }
+   
+}
+
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
+
+function keyPressed(){
+  if(key == "g"){
+    saveGif('thumb', 5);
+  } else if(key == "p"){
+    saveCanvas('thumb', "jpg");
+  }
+}
+
+class Cell {
+  constructor(x, y, w, h){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.on = false;
+  }
+
+  checkForFill(wave){
+    for(let i = 0; i < wave.length; i++){
+      // debugger;
+      if(wave[i].x > this.x && wave[i].x < this.x + this.w && wave[i].y > this.y && wave[i].y < this.y + this.h){
+        this.on = true;
+      }
+
+      if(wave[i].x > this.x + this.w){
+        return //stop checking
+      }
+    }
+    
+  }
+
+  draw(){
+    if(this.on){
+      fill(0);
+    } else {
+      fill(255);
+    }
+
+    rect(this.x, this.y, this.w, this.h);
+  }
+}
