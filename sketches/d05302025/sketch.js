077a0b0a881df3ae3c695b0e4a6d31f14cdaa7fe
Adam Driggers <awdriggs@gmail.com>
2025-06-03 15:41:12 +0800

    update

---
+++ sketches/d05302025/sketch.js
@@ -0,0 +1,83 @@
+let originalImage;
+let maskedImage;
+let mask;
+let angle = 0;
+
+function setup() {
+  createCanvas(800, 800);
+  // createCanvas(windowWidth, windowHeight);
+  makeMask();
+
+  resetBg();
+  
+  maskedImage = originalImage.get();
+  maskedImage.mask(mask);
+}
+
+function draw() {
+  background(255);
+  
+  fill(0);
+  //redraw the orginal image
+  image(originalImage, 0, 0);
+  
+  //rotate
+  push();
+  translate(width/2, height/2);
+  rotate(angle);
+  image(maskedImage, -width/2, -height/2);
+  pop();
+
+  angle+=0.01;
+}
+
+function keyPressed(){
+  if(key == "g"){
+    saveGif('thumb', 10.46);
+  } else if(key == "p"){
+    saveCanvas('thumb', "jpg");
+  }
+}
+
+function makeMask(){
+  pg = createGraphics(width, height);
+
+  // pg.background(255);
+  pg.fill(0, 255);
+  pg.ellipse(pg.width/2, pg.height/2, pg.width/2, pg.height/2)
+
+  mask = pg.get();
+}
+
+function resetBg(){
+  let bg = createGraphics(width, height);
+  
+  bg.background(255);
+  // bg.background("red");
+  
+  // bg.fill(0);
+  // bg.ellipse(width/4, height/4, width/3, height/3);
+  // let offset = width/8;
+
+  // let rectWidth = width/2;
+  // bg.rect(offset, 0, rectWidth - offset, height);
+  
+  bg.stroke(0);
+  bg.strokeWeight(width * 0.05);
+
+  bg.push();
+  bg.translate(width/2, height/2);
+  bg.rotate(random(0, TWO_PI));
+  
+  let amt = width/8;
+  let xJitter = random(-amt, amt);
+  let yJitter = random(-amt, amt);
+  bg.line(-width/2 + xJitter, -height/2 + yJitter, width/2 + xJitter, height/2 + yJitter); 
+  bg.pop();
+  
+  originalImage = bg.get();
+}
+
+ 
+
+
---
+++ sketches/d06032025/sketch.js
@@ -0,0 +1,104 @@
+let originalImage;
+let maskedImage;
+let mask;
+let angle = 0;
+
+function preload() {
+  // Code to run before the rest of the sketch.
+  originalImage = loadImage('manhole-1.png'); //load image
+  mask = loadImage('mask-1.png'); //load mask
+}
+
+function setup() {
+  createCanvas(1000, 667);
+  // createCanvas(windowWidth, windowHeight);
+  // makeMask();
+
+  // resetBg();
+  maskedImage = originalImage.get();
+  maskedImage.mask(mask);
+}
+
+function draw() {
+  background(255);
+
+  fill(0);
+  //redraw the orginal image
+  image(originalImage, 0, 0);
+
+
+
+  if (keyIsDown(LEFT_ARROW) || keyIsDown(RIGHT_ARROW)) {
+    if(key == "ArrowLeft"){
+      angle+=0.01;
+    } else if(key == "ArrowRight"){
+
+      angle-=0.01;
+    }
+    print(angle);
+    // Move diagonally.
+  }
+
+
+  //rotate
+  push();
+  translate(width/2, height/2);
+  rotate(angle);
+  image(maskedImage, -width/2, -height/2);
+
+  // fill(0, 255, 0);
+  // ellipse(0, 0, 20, 20);
+  pop();
+
+  let rotation = abs(angle % TWO_PI);
+  if(rotation > 0.54 && rotation < 0.57){
+    textSize(40);
+    text("✅", 40, 40);
+  }
+
+  // angle+=0.01;
+}
+
+function keyPressed(){
+  // if(key == "g"){
+  //   saveGif('thumb', 10.46);
+  // } else if(key == "p"){
+  //   saveCanvas('thumb', "jpg");
+  // }
+  print(key);
+  if(key == "ArrowLeft"){
+    angle+=0.01;
+  } else if(key == "ArrowRight"){
+
+    angle-=0.01;
+  }
+}
+
+function makeMask(){
+  pg = createGraphics(width, height);
+
+  // pg.background(255);
+  pg.fill(0, 255);
+  pg.ellipse(pg.width/2, pg.height/2, pg.width/2, pg.height/2)
+
+  mask = pg.get();
+}
+
+function resetBg(){
+  let bg = createGraphics(width, height);
+
+  bg.background(255);
+  // bg.background("red");
+
+  bg.fill(0);
+  // bg.ellipse(width/4, height/4, width/3, height/3);
+  let offset = width/8;
+  let rectWidth = width/2;
+  bg.rect(offset, 0, rectWidth - offset, height);
+
+  originalImage = bg.get();
+}
+
+
+
+
