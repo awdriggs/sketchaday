973b17ce013ba49794d608c6b05c04bc9916da74
Adam Driggers <awdriggs@gmail.com>
2025-08-10 20:50:54 +0200

    update

---
+++ sketches/d08082025/sketch.js
@@ -0,0 +1,119 @@
+//smaller inner square with a grid of squares
+
+let margin;
+let numCols, numRows;
+let innerWidth, innerHeight;
+let cellSize, borderSize;
+let nValue = 0;
+
+let colors = [];
+
+function setup() {
+  createCanvas(800, 800);
+  margin = width/6;
+  // createCanvas(windowWidth, windowHeight);
+
+  numRows = 20;
+  numCols = 20;
+
+
+  innerWidth = width - 2 * margin;
+  innerHeight = height - 2 * margin;
+
+  borderSize = innerWidth / numCols;
+  cellSize = borderSize * 0.5;
+
+  rectMode(CENTER);
+  noStroke();
+  blendMode(HARD_LIGHT)
+  // blendMode(SOFT_LIGHT)
+   
+  // blendMode(OVERLAY)
+
+  // strokeWeight(2.5);
+  for(let i = 0; i < numRows * numCols; i++){
+    let r = random(0, 255)
+    let g = random(0, 255)
+    let b = random(0, 255)
+    let a = 50 
+    colors.push(color(r, g, b, a));
+  }
+}
+
+function draw() {
+  background(255);
+
+  fill(0);
+
+  let grid = [];
+  for(let i = 0; i < numRows; i++){
+    let points = [];
+    for(let j = 0; j < numCols; j++){
+      let x = j * borderSize + margin + cellSize/2;
+      let y = i * borderSize + margin + cellSize/2;
+
+      let cIndex = j * numCols + i;
+      let c = colors[cIndex];
+
+      push();
+      translate(x,y);
+      noiseDetail(2, 0.75);
+      let angle = map(noise(j/100, i/100, nValue), 0, 1, -TWO_PI, TWO_PI);
+      rotate(angle);
+      fill(c);
+      rect(0,0, cellSize * 4, cellSize * 8, cellSize * 3);
+
+      // ellipse(0,0, 2, 2);
+
+
+      //left top corner
+      // rect(0 - cellSize, - cellSize, cellSize * 2, cellSize * 2);
+      // let rotatedCorner = localCorner.copy().rotate(angle);
+      // let globalCorner = p5.Vector.add(createVector(x, y), rotatedCorner);
+
+
+      // console.log(globalCorner.x, globalCorner.y);
+
+      
+      // points.push({x: globalCorner.x, y: globalCorner.y})
+
+      pop();
+
+      noFill();
+
+      // rect(globalCorner.x - borderSize, globalCorner.y - borderSize, borderSize * 2, borderSize * 2);
+      // fill("red");
+      // debugger;
+      // ellipse(points[j].x, points[j].y, 2, 2);
+      if(points.length > 1){
+        // stroke("red");
+        // line(points[j].x, points[j].y, points[j-1].x, points[j-1].y);
+      }
+
+      if(grid.length > 0){
+        // stroke("green");
+        // line(grid[i-1][j].x, grid[i-1][j].y, points[j].x, points[j].y);
+      }
+    }
+    
+    grid.push(points);
+
+    // debugger;
+      
+  }
+
+  nValue += 0.01;
+}
+
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
+
+function keyPressed(){
+  if(key == "g"){
+    saveGif('thumb', floor(random(3, 8)));
+  } else if(key == "p"){
+    saveCanvas('thumb', "jpg");
+  }
+}
+
---
+++ sketches/d08102025/sketch.js
@@ -0,0 +1,57 @@
+let rowHeight, distBeforeCurl, numRows;
+
+
+function setup() {
+  createCanvas(800, 800);
+  // createCanvas(windowWidth, windowHeight);
+
+  numRows = 10;
+  rowHeight = height/numRows;
+
+  distBeforeCurl = width * 0.15;
+
+  strokeWeight(1);
+
+
+}
+
+function draw() {
+  background(255);
+
+  let offset = -width/4;
+
+  for(let i = -numRows; i < numRows; i++){
+
+    let y = i * rowHeight * 1.5;
+    for(let x = offset; x < width; x+= distBeforeCurl - rowHeight * 2/3){
+      line(x, y, x + distBeforeCurl, y);
+      curl(x + distBeforeCurl, y, rowHeight);
+      y += rowHeight;
+    }
+  }
+
+}
+
+function curl(x, y, h){
+  let o1 = h * 1/3
+  let o2 = h * 2/3
+
+  line(x, y, x, y + o2);
+  line(x, y + o2, x - o1, y + o2);
+  line(x - o1, y + o2, x - o1, y + o1);
+  line(x - o1, y + o1, x - o2, y + o1);
+  line(x - o2, y + o1, x - o2, y + h);
+}
+
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
+
+function keyPressed(){
+  if(key == "g"){
+    saveGif('thumb', 5);
+  } else if(key == "p"){
+    saveCanvas('thumb', "jpg");
+  }
+}
+
