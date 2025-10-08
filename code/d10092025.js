fe499ed0df621bc72d646a7691ea7b78a5cb6b1e
Adam Driggers <awdriggs@gmail.com>
2025-10-08 20:00:20 +0200

    update

---
+++ code/d10092025.js
@@ -0,0 +1,200 @@
+
+let grid, emptyCells, agents;
+let numCols, numRows;
+
+function setup() {
+  createCanvas(800, 800);
+  // createCanvas(windowWidth, windowHeight);
+  numCols = 100;
+  numRows = 100;
+
+  start();
+
+  noStroke();
+}
+
+let lastNumEmpty;
+
+function draw() {
+  background(255);
+  emptyCells = []; //reset empty cells
+
+  for(let i = 0; i < grid.length; i++){
+    for(let j = 0; j < grid[i].length; j++){
+      let c = grid[i][j];
+      c.draw();
+
+      if(c.fill == 255){
+        emptyCells.push([i, j]); //if the cell is still empty add it back to the empty cell array
+      }
+    }
+  }
+
+  //loop through agents
+  for(let a of agents){
+    if(a.growX || a.growY){
+      a.grow();
+    }
+  }
+
+  // if(lastNumEmpty == emptyCells.length){
+  //   start();
+  // } else {
+  //   lastNumEmpty = emptyCells.length;
+  // }
+
+  // if(frameCount == 1){
+  //   saveGif('thumb', floor(random(3, 8)));
+  // }
+}
+
+function start(){
+  emptyCells = [];
+  grid = [];
+  agents = [];
+  
+  let cellSize = width/numCols;
+
+  for(let i = 0; i < numRows; i++){
+    let aRow = [];
+    let y = i * cellSize;
+    for(let j = 0; j < numCols; j++){
+
+      let x = j * cellSize;
+      aRow.push(new Cell(x, y, cellSize))
+
+      emptyCells.push([i, j]); //all cells start as empty so add it now
+
+    }
+    grid.push(aRow);
+  }
+
+  let numAgents = 10;
+  for(let i = 0; i < numAgents; i++){
+    agents.push(new Rectangle(10, 10));
+  }
+}
+
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
+
+function keyPressed(){
+  if(key == "g"){
+    saveGif('thumb', floor(random(3, 8)));
+  } else if(key == "p"){
+    saveCanvas('thumb', "jpg");
+  }
+}
+
+function findEmpty(){
+  //function to find all the empty cells
+}
+
+class Cell {
+  constructor(x, y, s) {
+    this.x = x;
+    this.y = y;
+    this.size = s;
+    this.fill = 255;
+  }
+
+  draw(){
+    fill(this.fill);
+    rect(this.x, this.y, this.size, this.size);
+  }
+}
+
+class Rectangle {
+  constructor(maxW, maxH){
+    this.maxWidth = maxW;
+    this.maxHeight = maxH;
+
+    this.init(); //initialize
+  }
+
+  init(){
+    //find and empty cell.
+    //empty cell is a 1d array of indices
+    let randomIndex = floor(random(emptyCells.length));
+    let cell = emptyCells[randomIndex];
+    this.startX = cell[1]
+    this.startY = cell[0]
+
+    this.vLoc = cell[0]
+    this.hLoc = cell[1]
+    this.w = 1;
+    this.h = 1;
+
+    this.growX = true;
+    this.growY = true;
+    this.growing = true;
+    this.color = color(random(0, 255), random(0,255), random(0,255));
+    // this.color = 0;
+
+    //claim cell
+    grid[this.vLoc][this.hLoc].fill = this.color;
+  }
+
+  grow(){
+
+    if(this.growX && this.hLoc + 1 < numCols){
+      let nextX = this.hLoc + 1;
+
+      //see if the new column can be added
+      let addCol = true; //assume yes
+      let cellsToFlip = [];
+      for(let i = 0; i < this.h; i++){
+        //cell to check
+        let cell = grid[this.startY + i][nextX];
+
+        if(cell.fill == 255){
+          cellsToFlip.push(cell); //add cell to array
+        } else { //any cell in the column is taken, stop
+          addCol = false;
+          break;
+        }
+      }
+
+      if(addCol){ //if the whole col can be added
+        this.hLoc++; //update to the new leading horiziontal position
+        this.w++; //update the width
+        for(let c of cellsToFlip){
+          c.fill = this.color;
+        }
+      } else {
+        this.growX = false; //stop trying to grow in the x location
+      }
+    }
+
+    //same logic for y
+    if(this.growY && this.vLoc + 1 < numRows){
+      let nextY = this.vLoc + 1;
+      
+      let addRow = true;
+      let cellsToFlip = [];
+
+      for(let i = 0; i < this.w; i++){
+        let cell = grid[nextY][this.startX + i];
+
+        if(cell.fill == 255){
+          cellsToFlip.push(cell);
+        } else {
+          addRow = false;
+          break
+        }
+      }
+
+      if(addRow){
+        this.vLoc++;
+        this.h++;
+        for(let c of cellsToFlip){
+          c.fill = this.color;
+        }
+      } else {
+        this.growY = false;
+      }
+    }
+  }
+
+}
