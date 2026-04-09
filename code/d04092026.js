b09970b8f01d37f72bdd636a5f98500a9ac7f929
Adam Driggers <awdriggs@gmail.com>
2026-04-09 12:44:05 +0200

    update

---
+++ code/d04092026.js
@@ -0,0 +1,70 @@
+//bauhaus color wheel 2
+let slider;
+let maxSize;
+let angle, speed;
+// let colors = ["black", "red", "yellow", "white"]
+let circs = [];
+let maxDistance, mouseDistance, maxSpeed;
+
+function setup() {
+  createCanvas(800, 800);
+  maxSize = width * 0.8;
+  angle = 0;
+  speed = 0.05;
+
+  let randomColor = color(random(255), random(255), random(255), 5);
+
+  let prevRadius = maxSize/2;
+  let cx = 0, cy = 0;
+  let ownRadius = prevRadius
+  let minSize = width/100;
+
+  while(ownRadius > width/200 && ownRadius > minSize){
+    ownRadius = prevRadius - random(2, minSize);
+    let maxOffset = prevRadius - ownRadius;
+
+    // pick a random position within the parent circle
+    let r = random(0, maxOffset);
+    let a = random(TWO_PI);
+    cx += r * cos(a);
+    cy += r * sin(a);
+
+    circs.push({color: randomColor, x: cx, y: cy, size: ownRadius * 2});
+
+    prevRadius = ownRadius;
+    // print(prevRadius)
+  }
+  
+  maxDistance = dist(0, 0, width, height); //calculate just once
+  maxSpeed = 0.2;
+  print(maxDistance)
+  noStroke();
+}
+
+function draw() {
+  background(255);
+  mouseDistance = dist(0, 0, mouseX, mouseY);
+  speed = map(mouseDistance, 0, maxDistance, 0, maxSpeed);
+
+  // fill(colors[0]);
+  // ellipse(width/2, height/2, maxSize, maxSize);
+
+  push();
+  translate(width/2, height/2);
+  rotate(angle);
+  for(let c of circs){
+    fill(c.color);
+    ellipse(c.x, c.y, c.size, c.size);
+  }
+  pop();
+
+  angle += speed;
+}
+
+function keyPressed(){
+  if(key == "g"){
+    saveGif('thumb', floor(random(3, 8)));
+  } else if(key == "p"){
+    saveCanvas('thumb', "jpg");
+  }
+}
